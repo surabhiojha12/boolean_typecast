8fd97cb09451e8e7a4c59b5823aac6043a270be9
Surabhi Ojha <surabhiojha12@gmail.com>
2021-06-30 23:52:09 +0530

    added to_bool code

--- lib/boolean_typecast.rb
+++ lib/boolean_typecast.rb
@@ -1,6 +1,21 @@
-require "boolean_typecast/version"
+require 'boolean_typecast/version'
 
+# Type conversions from anything to true/false
 module BooleanTypecast
-  class Error < StandardError; end
-  # Your code goes here...
+  # @example True values
+  #  true.to_bool?  #=> true
+  #  ''.to_bool?    #=> true
+  #  0.to_bool?     #=> true
+  #  {}.to_bool?    #=> true
+  #
+  # @example False values
+  #  false.to_bool? #=> false
+  #  nil.to_bool?   #=> false
+  def to_bool?
+    !!self
+  end
+end
+
+class Object
+  include BooleanTypecast
 end
--- test/boolean_typecast_test.rb
+++ test/boolean_typecast_test.rb
@@ -5,7 +5,16 @@ class BooleanTypecastTest < Minitest::Test
     refute_nil ::BooleanTypecast::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_that_it_converts_true_expressions_to_true
+    assert_equal true, true.to_bool?
+    assert_equal true, 0.to_bool?
+    assert_equal true, ''.to_bool?
+    assert_equal true, {}.to_bool?
+    assert_equal true, 0.0.to_bool?
+  end
+
+  def test_that_it_converts_false_expressions_to_false
+    assert_equal false, false.to_bool?
+    assert_equal false, nil.to_bool?
   end
 end
